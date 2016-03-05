93dda38d060e93f507092a31f84ef1d65268ebcd
Sreevatsav <sreevatsav.r@gmail.com>
2016-03-05 18:38:06 +0530

    Sreevatsav added

--- admin/alttlabs/modifyhotel.php
+++ admin/alttlabs/modifyhotel.php
@@ -2,7 +2,7 @@
 /*
  * modifyhotel.php
  * 
- * Copyright 2016 Raja Sekhar P <Raja Sekhar P@RAJASEKHAR-PC>
+ * Copyright 2016 Raja Sekhar P <Raja Sekhar P@RAJASEKHAR-PC> <Sreevatsav>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
